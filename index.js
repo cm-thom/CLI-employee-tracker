8425b129b4234501d37d5e10a926d0bcfb5f28b5
Cole <122782745+cm-thom@users.noreply.github.com>
2023-06-12 01:32:48 -0400

    added update functionality. tested all functions.

--- index.js
+++ index.js
@@ -9,62 +9,73 @@ const connection = mysql.createConnection({
     password: process.env.DB_PASSWORD 
 });
 
-inquirer.prompt([
-    {
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
-        name: 'userSelection'
-        
-    }]).then( data => {
-        switch(data.userSelection) {
-            case 'View All Departments':
-                viewAllDepartments();
-                break;
-            case 'View All Roles':
-                viewAllRoles();
-                break;
-            case 'View All Employees':
-                viewAllEmployees();
-                break;
-            case 'Add a Department':
-                addDepartment();
-                break;
-            case 'Add a Role':
-                addRole();
-                break;
-            case 'Add an Employee':
-                addEmployee();
-                break;
-            case 'Update an Employee Role':
-                updateEmployeeRole();
-                break;
-            default:
-                console.log('it\'s broken')
-                console.log(choice.userSelection)
-        }
-    });
+const prompt = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
+            name: 'userSelection'
+            
+        }]).then( data => {
+            switch(data.userSelection) {
+                case 'View All Departments':
+                    viewAllDepartments();
+                    break;
+                case 'View All Roles':
+                    viewAllRoles();
+                    break;
+                case 'View All Employees':
+                    viewAllEmployees();
+                    break;
+                case 'Add a Department':
+                    addDepartment();
+                    break;
+                case 'Add a Role':
+                    addRole();
+                    break;
+                case 'Add an Employee':
+                    addEmployee();
+                    break;
+                case 'Update an Employee Role':
+                    updateEmployeeRole();
+                    break;
+                default:
+                    console.log('it\'s broken')
+                    console.log(choice.userSelection)
+            }
+        });
+};
+
+prompt();
+
 
 const viewAllDepartments = () => {
     connection.query(
         'SELECT * FROM department',
-        (err, results) => console.table(results)
+        (err, results) => {console.table(results)
+        prompt();
+        }  
     )
 };
 
 const viewAllRoles = () => {
     connection.query(
         'SELECT * FROM role',
-        (err, results) => console.table(results)
+        (err, results) => {console.table(results)
+        prompt();
+        }
     )
 };
 
 const viewAllEmployees = () => {
     connection.query(
         'SELECT * FROM employee',
-        (err, results) => console.table(results)
-    )
-};
+        (err, results) => {console.table(results)
+            prompt();
+            }
+        )
+    };
 
 const addDepartment = () => {
     inquirer.prompt([
@@ -76,7 +87,10 @@ const addDepartment = () => {
     ]).then( (data) => {
         connection.query(
             `INSERT INTO department (department_name) VALUES ('${data.departmentName}')`,
-            (err, results) => console.log(`${data.departmentName} department added!`)
+            (err, results) => {
+                console.log(`${data.departmentName} department added!`)
+                prompt();
+            }
         )
     })
 };
@@ -86,15 +100,92 @@ const addRole = () => {
         {
             type: 'input',
             message: 'Enter role name:',
-            name: roleName
+            name: 'roleName'
+        },
+        {
+            type: 'number',
+            message: 'Enter role salary:',
+            name: 'roleSalary'
+        },
+        {
+            type: 'integer',
+            message: 'Enter department ID of new role:',
+            name: 'roleDepartmentId'
         }
-    ])
+    ]).then( (data) => {
+        console.log(data.roleSalary);
+        console.log(data.roleDepartmentId);
+        connection.query(
+            `INSERT INTO role (title, salary, department_id) VALUES ('${data.roleName}', ${data.roleSalary}, ${data.roleDepartmentId})`,
+            (err, results) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log(`${data.roleName} role added!`)
+                    prompt();
+                }}
+        )
+    })
 };
 
 const addEmployee = () => {
-    //logic
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter employee first name:',
+            name: 'firstName'
+        },
+        {
+            type: 'input',
+            message: 'Enter employee last name:',
+            name: 'lastName'
+        },
+        {
+            type: 'integer',
+            message: 'Enter role ID of the employee:',
+            name: 'roleId'
+        },
+        {
+            type: 'integer',
+            message: 'Enter manager ID of the employee:',
+            name: 'managerId'
+        }
+    ]).then( (data) => {
+        connection.query(
+            `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${data.firstName}', '${data.lastName}', ${data.roleId}, ${data.managerId})`,
+            (err, results) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log(`New employee added!`)
+                    prompt();
+                }}
+        )
+    })
 };
 
 const updateEmployeeRole = () => {
-    //logic
+    inquirer.prompt([
+        {
+            type: 'number',
+            message: 'Enter the ID of the employee whose role will be updated:',
+            name: 'employeeId'
+        },
+        {
+            type: 'number',
+            message: 'Enter the ID of the updated role:',
+            name: 'roleId'
+        }
+    ]).then( (data) => {
+        connection.query(
+            `UPDATE employee SET role_id = ${data.roleId} WHERE id = ${data.employeeId}`, (err, results) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log(`Employee role updated!`)
+                    prompt();
+                }
+            }
+        )
+    })
 };
