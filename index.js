568d0a42d8edd699608892d0b0d59eb265e20768
Cole <122782745+cm-thom@users.noreply.github.com>
2023-06-11 23:40:58 -0400

    built out switch case. added templates for functionality

--- index.js
+++ index.js
@@ -24,8 +24,54 @@ inquirer.prompt([
             case 'View All Departments':
                 viewAllDepartments();
                 break;
+            case 'View All Roles':
+                viewAllRoles();
+                break;
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+            case 'Add a Department':
+                addDepartment();
+                break;
+            case 'Add a Role':
+                addRole();
+                break;
+            case 'Add an Employee':
+                addEmployee();
+                break;
+            case 'Update an Employee Role':
+                updateEmployeeRole();
+                break;
             default:
                 console.log('it\'s broken')
                 console.log(choice.userSelection)
         }
     });
+
+const viewAllDepartments = () => {
+    //logic
+}
+
+const viewAllRoles = () => {
+    //logic
+}
+
+const viewAllEmployees = () => {
+    //logic
+}
+
+const addDepartment = () => {
+    //logic
+}
+
+const addRole = () => {
+    //logic
+}
+
+const addEmployee = () => {
+    //logic
+}
+
+const updateEmployeeRole = () => {
+    //logic
+}
